653433beb8289fb7350ac90e5f0309bcb99ed8cc
wenbo9045 <1425163348@qq.com>
2025-07-04 15:21:14 +0800

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-# 更新依赖
-apt-get update
-rosdep update
 
-# 创建工作空间
-mkdir -p catkin_ws/src
-cd catkin_ws
-catkin_make
-source devel/setup.bash
+set -e
 
-# 克隆示例代码 (替换为你的实际仓库)
-git clone https://github.com/wenbo9045/traffic_flow_trajectory_planning.git
-rosdep install --from-paths src --ignore-src -y
-catkin_make -DCMAKE_BUILD_TYPE=Release
+echo "[setup] Updating apt"
+apt update && apt install -y ros-noetic-pcl-ros python3-pip python3-dev python3-venv
+
+pip install rospkg
+
+echo "[setup] Installing Gym"
+pip install gym
+pip install pygame
+
+echo "[setup] Installing additional Python dependencies"
+pip install numpy matplotlib rospkg catkin_pkg
+
+echo "[setup] Setting up ROS environment"
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source /opt/ros/noetic/setup.bash
+
+echo "[setup] Creating catkin workspace"
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws && catkin_make
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
